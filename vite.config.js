634fb956fd03b0e9cf70fe5fea5d6e8bf708042c
r0c0y <ranchoguruji07@gmail.com>
2025-06-13 01:54:14 +0530

    Fix CSP issue for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-// https://vite.dev/config/
+import { createHtmlPlugin } from 'vite-plugin-html'
+
 export default defineConfig({
-  base: '/NestFindr/', // 👈 repo name here!
-  plugins: [react()],
+  base: '/NestFindr/', // ✅ for GitHub Pages
+  plugins: [
+    react(),
+    createHtmlPlugin({
+      inject: {
+        data: {
+          csp: `<meta http-equiv="Content-Security-Policy" content="default-src 'self'; font-src 'self' data: https:; script-src 'self' https:; connect-src 'self' https://*.firebaseio.com https://*.googleapis.com https://*.firebaseapp.com https://*.gstatic.com; style-src 'self' 'unsafe-inline' https:;">`
+        }
+      }
+    })
+  ]
 })
